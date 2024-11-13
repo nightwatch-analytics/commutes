0fd83f390a7f0c833993702356b719e7336e5525
Joseph Mohr <jmohr@thenightwatch.co>
2024-11-12 23:24:56 -0600

    Add toolkit

---
+++ R/toolkit.R
@@ -0,0 +1,24 @@
+# Setup ----
+library(janitor)
+library(lehdr)
+library(sf)
+library(tidycensus)
+library(tidyverse)
+
+# Import ----
+
+
+# Tidy ----
+
+
+# Transform ----
+
+
+# Visualize ----
+
+
+# Model ----
+
+
+# Communicate ----
+
