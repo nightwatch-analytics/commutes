33b8f55de80f5b9f02ed74be694b9f2b904dcd17
Joseph Mohr <jmohr@thenightwatch.co>
2024-11-21 11:47:41 -0600

    Update toolkit

--- R/toolkit.R
+++ R/toolkit.R
@@ -4,9 +4,22 @@ library(lehdr)
 library(sf)
 library(tidycensus)
 library(tidyverse)
+library(tigris)
+
+options(tigris_use_cache = TRUE)
 
 # Import ----
-
+get_lodes <- function(state) {
+  grab_lodes(state = state,
+             year = 2021,
+             lodes_type = "od",
+             job_type = "JT01",
+             segment = "S000",
+             agg_geo = "tract",
+             state_part = "main",
+             use_cache = T) %>%
+    select(w_tract, h_tract, S000)
+}
 
 # Tidy ----
 
